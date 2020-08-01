269085e7048495cdb6f81609161bc9d096473761
u89man <u89man@gmail.com>
2020-08-01 21:33:32 +0500

    Инициализация проекта

---
+++ readme.md
@@ -0,0 +1,234 @@
+## Filesystem 0.2.0
+
+##### Установка
+
+```
+composer require u89man/filesystem
+```
+
+##### Примеры
+
+```php
+
+$fs = new \U89Man\Filesystem\Fs();
+```
+```php
+// Обновляет время последнего доступа к файлу
+$fs->touch('file.txt'); // bool
+
+// Обновляет время последнего доступа к файлу на указанное
+$fs->touch('file.txt', 1596297826); // bool
+
+// Обновляет время последнего доступа к каталогу 
+$fs->touch('/dir'); // bool
+
+// Обновляет время последнего доступа к каталогу на указанное
+$fs->touch('/dir', 1596297826); // bool
+
+// Создает новый файл, если указанный файл или каталог не существует
+$fs->touch('non_existent_file.txt'); // bool
+```
+```php
+// Создает новый файл
+$fs->createFile('file.txt'); // bool
+
+// Создает новый файл с указанием прав доступа
+$fs->createFile('file.txt', 0644); // bool
+$fs->createFile('file.txt', Fs::MODE_FILE_PRIVATE); // bool
+$fs->createFile('file.txt', Fs::MODE_FILE_PUBLIC); // bool
+```
+```php
+// Создает новый каталог
+$fs->createDir('/dir'); // bool
+
+// Создает новый каталог с указанием прав доступа
+$fs->createDir('/dir', 0755); // bool
+$fs->createDir('/dir', Fs::MODE_DIRECTORY_PRIVATE); // bool
+$fs->createDir('/dir', Fs::MODE_DIRECTORY_PUBLIC); // bool
+```
+```php
+// Читает содержимое файла в строку
+$content = $fs->readFile('file.txt'); // string
+
+// Читает содержимое файла в строку, блокируя доступ к файлу на время чтения
+$content = $fs->readFile('file.txt', true); // string
+```
+```php
+// Построчно читает содержимое файла в массив
+$linesContent = $fs->readFileLines('file.txt'); // array
+
+// Построчно читает содержимое файла в массив, пропуская пустые строки
+$linesContent = $fs->readFileLines('file.txt', true); // array
+```
+```php
+// Записывает содержимое в файл
+$fs->writeFile('file.txt', $content); // bool
+
+// Записывает содержимое в файл, блокируя доступ к файлу на время записи
+$fs->writeFile('file.txt', $content, true); // bool
+```
+```php
+// Дописывает содержимое в конец файла
+$fs->appendFile('file.txt', $appendContent); // bool
+
+// Дописывает содержимое в конец файла, блокируя доступ к файлу на время записи
+$fs->appendFile('file.txt', $appendContent, true); // bool
+```
+```php
+// Дописывает содержимое в начало файла
+$fs->prependFile('file.txt', $prependContent); // bool
+
+// Дописывает содержимое в начало файла, блокируя доступ к файлу на время записи
+$fs->prependFile('file.txt', $prependContent, true); // bool
+```
+```php
+// Получает размер файла (в байтах)
+$size = $fs->size('file.txt'); // int
+
+// Получает размер каталога (в байтах)
+$size = $fs->size('/dir'); // int
+```
+```php
+// Получает хеш файла (md5)
+$hash = $fs->hashFile('file.txt'); // string
+
+// Получает хеш файла (md5)
+$hash = $fs->hashFile('file.txt', Fs::HASH_MD5); // string
+
+// Получает хеш файла (sha1)
+$hash = $fs->hashFile('file.txt', Fs::HASH_SHA1); // string
+```
+```php
+// Получает содержимое каталога в виде массива имен (файлы и каталоги)
+$listDir = $fs->listDir('/dir'); // array
+
+// Получает содержимое каталога в виде массива имен (только файлы)
+$listDir = $fs->listDir('/dir', true); // array
+
+// Получает содержимое каталога в виде массива имен (только файлы с конкретным расширением)
+$listDir = $fs->listDir('/dir', true, 'txt'); // array
+
+// Получает содержимое каталога в виде массива имен (только файлы с конкретными расширениями)
+$listDir = $fs->listDir('/dir', true, ['js', 'css']); // array
+```
+```php
+// Переименовывает файл
+$fs->rename('from.txt', 'to.txt'); // bool
+
+// Переименовывает каталог
+$fs->rename('/dir_from', '/dir_to'); // bool
+```
+```php
+// Копирует содержимое файла
+$fs->copy('from.txt', 'to.txt'); // bool
+
+// Копирует содержимое файла с заменой 
+$fs->copy('from.txt', 'to.txt', true); // bool
+
+// Копирует содержимое каталога
+$fs->copy('/dir_from', '/dir_to'); // bool
+
+// Копирует содержимое каталога c заменой
+$fs->copy('/dir_from', '/dir_to', true); // bool
+```
+```php
+// Перемещает файл 
+$fs->move('from.txt', 'to.txt'); // bool
+
+// Перемещает файл с заменой
+$fs->move('from.txt', 'to.txt', true); // bool
+
+// Перемещает каталог
+$fs->move('/dir_from', '/dir_to'); // bool
+
+// Перемещает каталог с заменой
+$fs->move('/dir_from', '/dir_to', true); // bool
+```
+```php
+// Удаляет файл
+$fs->delete('file.txt'); // bool
+
+// Удаляет каталог
+$fs->delete('/dir'); // bool
+
+// Удаляет каталог со всем содержимым
+$fs->delete('/dir', true); // bool
+```
+```php
+// Получает Mimetype файла
+$mimetype = $fs->mimetype('file.txt'); // string "text/plain"
+$mimetype = $fs->mimetype('/dir'); // string "directory"
+```
+```php
+// Устанавливает права доступа на файл
+$fs->chmod('file.txt', 0644); // bool
+$fs->chmod('file.txt', Fs::MODE_FILE_PRIVATE); // bool
+$fs->chmod('file.txt', Fs::MODE_FILE_PUBLIC); // bool
+
+// Устанавливает права доступа каталог
+$fs->chmod('/dir', 0755); // bool
+$fs->chmod('/dir', Fs::MODE_DIRECTORY_PRIVATE); // bool
+$fs->chmod('/dir', Fs::MODE_DIRECTORY_PUBLIC); // bool
+
+// Получает права доступа к файлу
+$chmod = $fs->chmod('file.txt'); // string
+
+// Получает права доступа к каталогу
+$chmod = $fs->chmod('/dir'); // string
+```
+```php
+// Получает тип файла
+$type = $fs->type('file.txt'); // string "file"
+$type = $fs->type('/dir'); // string "dir"
+```
+```php
+// Проверяет существование файла
+$fs->exists('file.txt'); // bool
+
+// Проверяет существование каталога
+$fs->exists('/dir'); // bool
+```
+```php
+// Проверяет является ли переданный путь файлом
+$fs->isFile('file.txt'); // bool
+```
+```php
+// Проверяет является ли переданный путь каталогом
+$fs->isDir('/dir'); // bool
+```
+```php
+// Проверяет доступность файла для чтения
+$fs->isReadable('file.txt'); // bool
+
+// Проверяет доступность каталога для чтения
+$fs->isReadable('/dir'); // bool
+```
+```php
+// Проверяет доступность файла для записи
+$fs->isWritable('file.txt'); // bool
+
+// Проверяет доступность каталога для записи
+$fs->isWritable('/dir'); // bool
+```
+```php
+// Получает время последнего доступа к файлу
+$time = $fs->atime('file.txt'); // int
+
+// Получает время последнего доступа к каталогу
+$time = $fs->atime('/dir'); // int
+```
+```php
+// Получает время последнего изменения индексного дескриптора файла
+$time = $fs->ctime('file.txt'); // int
+
+// Получает время последнего изменения индексного дескриптора каталога
+$time = $fs->ctime('/dir'); // int
+```
+```php
+// Получает время последней модификации файла
+$time = $fs->mtime('file.txt'); // int
+
+// Получает время последней модификации каталога
+$time = $fs->mtime('/dir'); // int
+```
+
---
+++ src/Fs.php
@@ -0,0 +1,498 @@
+<?php
+
+namespace U89Man\Filesystem;
+
+use FilesystemIterator;
+
+class Fs
+{
+    /**
+     * Типы хешей.
+     */
+    const HASH_MD5  = 'md5';
+    const HASH_SHA1 = 'sha1';
+
+    /**
+     * Права доступа к файлам и каталогам.
+     */
+    const MODE_FILE_PRIVATE = 0600;
+    const MODE_FILE_PUBLIC = 0644;
+    const MODE_DIRECTORY_PRIVATE = 0700;
+    const MODE_DIRECTORY_PUBLIC = 0755;
+
+
+    /**
+     * Устанавливает/обновляет время доступа и модификации файла.
+     * Если файл или каталог не существует, будет создан пустой файл по указанному пути.
+     *
+     * @param string $path
+     * @param int|null $time
+     *
+     * @return bool
+     */
+    public function touch($path, $time = 0)
+    {
+        return touch($path, $time > 0 ? $time : time());
+    }
+
+    /**
+     * Создает файл.
+     *
+     * @param string $path
+     * @param int $mode 0xxx
+     *
+     * @return bool
+     */
+    public function createFile($path, $mode = self::MODE_FILE_PUBLIC)
+    {
+        return $this->writeFile($path, '', true) && $this->chmod($path, $mode);
+    }
+
+    /**
+     * Создает каталог.
+     *
+     * @param string $path
+     * @param int $mode 0xxx
+     * @param bool $recursive
+     *
+     * @return bool
+     */
+    public function createDir($path, $mode = self::MODE_DIRECTORY_PUBLIC, $recursive = false)
+    {
+        return mkdir($path, $mode, $recursive);
+    }
+
+    /**
+     * Читает содержимое из файла в строку.
+     *
+     * @param string $path
+     * @param bool $lock
+     *
+     * @return string
+     */
+    public function readFile($path, $lock = false)
+    {
+        $content = null;
+
+        if ($lock) {
+            if ($f = fopen($path, 'rb')) {
+                try {
+                    if (flock($f, LOCK_SH)) {
+                        clearstatcache(true, $path);
+
+                        $content = fread($f, $this->size($path) ?: 1);
+
+                        flock($f, LOCK_UN);
+                    }
+                } finally {
+                    fclose($f);
+                }
+            }
+        } else {
+            $content = file_get_contents($path);
+        }
+
+        if ($content) {
+            return $content;
+        }
+
+        return '';
+    }
+
+    /**
+     * Читает содержимое файла построчно и помещает его в массив.
+     *
+     * @param string $path
+     * @param bool $skipEmpty
+     *
+     * @return array
+     */
+    public function readFileLines($path, $skipEmpty = false)
+    {
+        return file($path, ($skipEmpty ? FILE_SKIP_EMPTY_LINES : 0) | FILE_IGNORE_NEW_LINES);
+    }
+
+    /**
+     * Обеспечивает существование каталога.
+     *
+     * @param string $path
+     *
+     * @return void
+     */
+    protected function ensureDirectoryExists($path)
+    {
+        $dir = dirname($path);
+
+        if ($this->isDir($dir)) {
+            return;
+        }
+
+        $this->createDir($dir, self::MODE_DIRECTORY_PUBLIC,true);
+    }
+
+    /**
+     * Записывает содержимое в файл.
+     *
+     * @param string $path
+     * @param string $content
+     * @param bool $lock
+     *
+     * @return bool
+     */
+    public function writeFile($path, $content, $lock = false)
+    {
+        $this->ensureDirectoryExists($path);
+
+        return file_put_contents($path, $content, ($lock ? LOCK_EX : 0)) !== false;
+    }
+
+    /**
+     * Записывает содержимое в конец файла.
+     *
+     * @param string $path
+     * @param string $content
+     * @param bool $lock
+     *
+     * @return bool
+     */
+    public function appendFile($path, $content, $lock = false)
+    {
+        $this->ensureDirectoryExists($path);
+
+        return file_put_contents($path, $content, (($lock ? LOCK_EX : 0 ) | FILE_APPEND)) !== false;
+    }
+
+    /**
+     * Записывает содержимое в начало файла.
+     *
+     * @param string $path
+     * @param string $content
+     * @param bool $lock
+     *
+     * @return bool
+     */
+    public function prependFile($path, $content, $lock = false)
+    {
+        $content = $content.($this->exists($path) ? $this->readFile($path, $lock) : '');
+
+        return $this->writeFile($path, $content, $lock);
+    }
+
+    /**
+     * Получает размер файла или каталога.
+     *
+     * @param string $path
+     *
+     * @return int
+     */
+    public function size($path)
+    {
+        $size = 0;
+
+        if ($this->isDir($path)) {
+            foreach ($this->listDir($path) as $file) {
+                $size += $this->size($path.'/'.$file);
+            }
+        } elseif ($this->isFile($path)) {
+            $size = filesize($path);
+        }
+
+        return (int) $size;
+    }
+
+    /**
+     * Получает содержимое каталога в виде массива имен файлов и каталогов.
+     *
+     * @param string $path
+     * @param bool $onlyFiles
+     * @param string|string[]|null $extensions
+     *
+     * @return array
+     */
+    public function listDir($path, $onlyFiles = false, $extensions = null)
+    {
+        if (! is_null($extensions)) {
+            $extensions = is_array($extensions) ? $extensions : array($extensions);
+        }
+
+        $files = [];
+
+        foreach (new FilesystemIterator($path) as $item) {
+            if ($onlyFiles) {
+                if ($item->isDir()) {
+                    continue;
+                }
+
+                if (! is_null($extensions) && ! in_array($item->getExtension(), $extensions)) {
+                    continue;
+                }
+            }
+            $files[] = $item->getFileName();
+        }
+
+        return $files;
+    }
+
+    /**
+     * Получает хеш файла.
+     *
+     * @param string $path
+     * @param string $type
+     *
+     * @return false|string
+     */
+    public function hashFile($path, $type = self::HASH_MD5)
+    {
+        switch ($type) {
+            case self::HASH_SHA1:
+                return sha1_file($path);
+            case self::HASH_MD5:
+            default:
+                return md5_file($path);
+        }
+    }
+
+    /**
+     * Переименовывает файл или каталог.
+     *
+     * @param string $fromPath
+     * @param string $toPath
+     *
+     * @return bool
+     */
+    public function rename($fromPath, $toPath)
+    {
+        return rename($fromPath, $toPath);
+    }
+
+    /**
+     * Удаляет файл или каталог.
+     *
+     * @param string $path
+     * @param bool $recursive
+     *
+     * @return bool
+     */
+    public function delete($path, $recursive = false)
+    {
+        if ($this->isDir($path)) {
+            $listDir = $this->listDir($path);
+
+            if (! $recursive && ! empty($listDir)) {
+                return false;
+            }
+
+            foreach (array_reverse($listDir) as $item) {
+                $this->delete($path.'/'.$item, $recursive);
+            }
+
+            return rmdir($path);
+        }
+
+        return unlink($path);
+    }
+
+    /**
+     * Копирует файл или каталог.
+     *
+     * @param string $fromPath
+     * @param string $toPath
+     * @param bool $replace
+     *
+     * @return bool
+     */
+    protected function _copy($fromPath, $toPath, $replace = false)
+    {
+        if ($this->exists($toPath) && ! $replace) {
+            return false;
+        }
+
+        if ($this->isFile($fromPath)) {
+            copy($fromPath, $toPath);
+        }
+        elseif ($this->isDir($fromPath)) {
+            if ($this->exists($toPath)) {
+                $this->delete($toPath, true);
+            }
+
+            $this->createDir($toPath);
+
+            foreach ($this->listDir($fromPath) as $path) {
+                if (! $this->_copy($fromPath.'/'.$path, $toPath.'/'.$path, $replace)) {
+                    return false;
+                }
+            }
+        }
+
+        $this->chmod($toPath, $this->chmod($fromPath));
+
+        return $this->size($fromPath) === $this->size($toPath);
+    }
+
+    /**
+     * Копирует файл или каталог.
+     *
+     * @param string $fromPath
+     * @param string $toPath
+     * @param bool $replace
+     *
+     * @return bool
+     */
+    public function copy($fromPath, $toPath, $replace = false)
+    {
+        return $this->_copy($fromPath, $toPath, $replace);
+    }
+
+    /**
+     * Перемещает файл или каталог.
+     *
+     * @param $fromPath
+     * @param $toPath
+     * @param bool $replace
+     *
+     * @return bool
+     */
+    public function move($fromPath, $toPath, $replace = false)
+    {
+        return $this->_copy($fromPath, $toPath, $replace) && $this->delete($fromPath, true);
+    }
+
+    /**
+     * Получает MIME-тип файла.
+     *
+     * @param string $path
+     *
+     * @return mixed
+     */
+    public function mimetype($path)
+    {
+        return mime_content_type($path);
+    }
+
+    /**
+     * Получает или устанавливает права доступа к файлу или каталогу.
+     *
+     * @param string $path
+     * @param int $mode 0xxx
+     *
+     * @return bool|string
+     */
+    public function chmod($path, $mode = 0)
+    {
+        if ($mode > 0) {
+            return chmod($path, intval($mode, 8));
+        }
+
+        return substr(decoct(fileperms($path)), -4);
+    }
+
+    /**
+     * Получает тип файла.
+     * Возможными значениями являются fifo, char, dir, block, link, file, socket и unknown.
+     *
+     * @param string $path
+     *
+     * @return false|string
+     */
+    public function type($path)
+    {
+        return filetype($path);
+    }
+    
+    /**
+     * Проверяет существование указанного файла или каталога.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function exists($path)
+    {
+        return file_exists($path);
+    }
+
+    /**
+     * Определяет, является ли указанный путь файлом.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function isFile($path)
+    {
+        return is_file($path);
+    }
+
+    /**
+     * Определяет, является ли указанный путь каталогом.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function isDir($path)
+    {
+        return is_dir($path);
+    }
+
+    /**
+     * Определяет доступность файла или каталога для чтения.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function isReadable($path)
+    {
+        return is_readable($path);
+    }
+
+    /**
+     * Определяет доступность файла или каталога для записи.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function isWritable($path)
+    {
+        return is_writable($path);
+    }
+
+    /**
+     * Получает время последнего доступа к файлу или каталогу.
+     *
+     * @param string $path
+     *
+     * @return int
+     */
+    public function atime($path)
+    {
+        return (int) fileatime($path);
+    }
+
+    /**
+     * Получает время изменения индексного дескриптора файла или каталога.
+     *
+     * @param string $path
+     *
+     * @return int
+     */
+    public function ctime($path)
+    {
+        return (int) filectime($path);
+    }
+
+    /**
+     * Получает время последнего изменения файла или каталога.
+     *
+     * @param string $path
+     *
+     * @return int
+     */
+    public function mtime($path)
+    {
+        return (int) filemtime($path);
+    }
+}
+
